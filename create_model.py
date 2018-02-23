315025a6163bef559abfd5f103fbbeb5ac16cbe3
xiaoxiaokouer <36734728+xiaoxiaokouer@users.noreply.github.com>
2018-02-23 22:31:32 +0800

    adjust parameters
    1. remove the last activation function "tanh";
    2. change the learn rate to 0.001
    .....
    the results:
    Epoch 1/20
    21600/21600 [==============================] - 638s 30ms/step - loss: 21.6030 - val_loss: 4.8996
    Epoch 2/20
    21600/21600 [==============================] - 632s 29ms/step - loss: 21.1742 - val_loss: 4.8679
    Epoch 3/20
    21600/21600 [==============================] - 632s 29ms/step - loss: 21.2323 - val_loss: 7.0474
    Epoch 4/20
    21600/21600 [==============================] - 643s 30ms/step - loss: 21.1050 - val_loss: 5.4899
    Epoch 5/20
    21600/21600 [==============================] - 660s 31ms/step - loss: 21.0987 - val_loss: 7.9370
    Epoch 6/20
    21600/21600 [==============================] - 663s 31ms/step - loss: 21.0948 - val_loss: 4.7083
    Epoch 7/20
    21600/21600 [==============================] - 643s 30ms/step - loss: 21.0941 - val_loss: 6.3591
    Epoch 8/20
    21600/21600 [==============================] - 630s 29ms/step - loss: 21.1166 - val_loss: 5.4436
    Epoch 9/20
    21600/21600 [==============================] - 630s 29ms/step - loss: 21.0537 - val_loss: 4.5315
    Epoch 10/20
    21600/21600 [==============================] - 630s 29ms/step - loss: 21.0617 - val_loss: 5.5277
    Epoch 11/20
    21600/21600 [==============================] - 630s 29ms/step - loss: 21.0572 - val_loss: 5.7546
    Epoch 12/20
    21600/21600 [==============================] - 630s 29ms/step - loss: 21.0674 - val_loss: 4.7885
    Epoch 13/20
    21600/21600 [==============================] - 646s 30ms/step - loss: 21.0518 - val_loss: 4.6803
    Epoch 14/20
    21600/21600 [==============================] - 654s 30ms/step - loss: 21.0493 - val_loss: 6.4155
    Epoch 15/20
    21600/21600 [==============================] - 649s 30ms/step - loss: 21.0530 - val_loss: 9.0618
    Epoch 16/20
    21600/21600 [==============================] - 643s 30ms/step - loss: 21.0702 - val_loss: 8.3877
    Epoch 17/20
    21600/21600 [==============================] - 644s 30ms/step - loss: 21.1499 - val_loss: 6.6487
    Epoch 18/20
    21600/21600 [==============================] - 643s 30ms/step - loss: 21.0940 - val_loss: 6.2951
    Epoch 19/20
    21600/21600 [==============================] - 644s 30ms/step - loss: 21.0628 - val_loss: 7.1452
    Epoch 20/20
    21600/21600 [==============================] - 644s 30ms/step - loss: 21.0558 - val_loss: 8.7329

--- create_model.py
+++ create_model.py
@@ -17,6 +17,7 @@ y_val = np.load('y_val.npy')
 from keras.models import Sequential
 from keras.layers.core import Lambda
 from keras.layers import Conv2D
+from keras.layers import MaxPooling2D
 from keras.layers import Flatten
 from keras.layers import Dense
 from keras.optimizers import Adam
@@ -32,24 +33,33 @@ def mj_model():
     input_shape = (FLAGS.img_h, FLAGS.img_w, FLAGS.img_c)
     model = Sequential()
     model.add(Lambda(lambda x:x/255.-0.5, input_shape=input_shape))
-    model.add(Conv2D(24,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Conv2D(36,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Conv2D(48,5,5,border_mode='valid', activation='relu', subsample=(2,2)))
-    model.add(Conv2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
-    model.add(Conv2D(64,3,3,border_mode='valid', activation='relu', subsample=(1,1)))
+    
+    model.add(Conv2D(24,5,5,padding='same', activation='relu'))
+#    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
+    model.add(Conv2D(36,5,5,padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
+    model.add(Conv2D(48,5,5,padding='same', activation='relu'))
+#    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
+    model.add(Conv2D(64,3,3,padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
+    model.add(Conv2D(64,3,3,padding='same', activation='relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
+    
     model.add(Flatten())
+    
     model.add(Dense(1164, activation='relu'))
     model.add(Dense(100, activation='relu'))
     model.add(Dense(50, activation='relu'))
-    model.add(Dense(10, activation='relu'))
-    model.add(Dense(1, activation='tanh'))
-    adam = Adam(lr=0.0001, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-    model.compile(optimizer=adam, loss="mse", metrics=['accuracy'])
+#    model.add(Dense(10, activation='relu'))  # need delete
+    model.add(Dense(1, activation=None))
+    
+    adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
+    model.compile(optimizer=adam, loss="mse")
     return model
 
 
 model = mj_model()  
-model.fit(x=X_train, y=y_train, batch_size=128, epochs=5, verbose=1, 
+model.fit(x=X_train, y=y_train, batch_size=64, epochs=20, verbose=1, 
           callbacks=None, validation_split=0.0, validation_data=(X_val,y_val), 
           shuffle=True, class_weight=None, sample_weight=None, 
           initial_epoch=0, steps_per_epoch=None, validation_steps=None)
