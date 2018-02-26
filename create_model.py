e1c41cb76004b42dc817c84f1c60d5809e01f9ba
xiaoxiaokouer <36734728+xiaoxiaokouer@users.noreply.github.com>
2018-02-26 18:36:49 +0800

    totally use NVIDIA model
    Train on 21600 samples, validate on 2700 samples
    Epoch 1/20
    21600/21600 [==============================] - 84s 4ms/step - loss: 21.6026 - val_loss: 2.4681
    Epoch 2/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.2236 - val_loss: 2.4069
    Epoch 3/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.1820 - val_loss: 2.0090
    Epoch 4/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.1466 - val_loss: 2.2897
    Epoch 5/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.1442 - val_loss: 2.0862
    Epoch 6/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.1000 - val_loss: 2.0516
    Epoch 7/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0811 - val_loss: 1.9525
    Epoch 8/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0986 - val_loss: 1.9541
    Epoch 9/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.1014 - val_loss: 1.9778
    Epoch 10/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0468 - val_loss: 1.9819
    Epoch 11/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.1271 - val_loss: 2.1644
    Epoch 12/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.1234 - val_loss: 1.9735
    Epoch 13/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0791 - val_loss: 2.1349
    Epoch 14/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0888 - val_loss: 2.0233
    Epoch 15/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0685 - val_loss: 2.0150
    Epoch 16/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0531 - val_loss: 2.0093
    Epoch 17/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0515 - val_loss: 1.9616
    Epoch 18/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.1462 - val_loss: 1.9648
    Epoch 19/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0773 - val_loss: 2.0381
    Epoch 20/20
    21600/21600 [==============================] - 80s 4ms/step - loss: 21.0737 - val_loss: 2.0024

--- create_model.py
+++ create_model.py
@@ -5,13 +5,18 @@ import string
 import cv2
 import os
 import numpy as np
+from sklearn.utils import shuffle 
 
 
-#----------------------------load data--------------------------------###
+#-----------------------load data and shufffle data------------------------###
 X_train = np.load('X_train.npy')
 y_train = np.load('y_train.npy')
 X_val = np.load('X_val.npy')
 y_val = np.load('y_val.npy')
+X_test = np.load('X_test.npy')
+y_test = np.load('y_test.npy')
+
+X_train, y_train = shuffle(X_train, y_train)
 
 ##-------------------------------build model--------------------------------##
 from keras.models import Sequential
@@ -36,9 +41,9 @@ def mj_model():
     
     model.add(Conv2D(24,kernel_size=5,strides=(2,2),padding='same', activation='relu'))
     
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
+#    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
     
-#    model.add(Conv2D(36,kernel_size=5,strides=(2,2),padding='same', activation='relu'))
+    model.add(Conv2D(36,kernel_size=5,strides=(2,2),padding='same', activation='relu'))
 #    
 #    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
     
@@ -48,9 +53,9 @@ def mj_model():
     
     model.add(Conv2D(64,kernel_size=3,strides=(1,1),padding='same', activation='relu'))
     
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
+#    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
     
-#    model.add(Conv2D(64,kernel_size=3,strides=(1,1),padding='same', activation='relu'))
+    model.add(Conv2D(64,kernel_size=3,strides=(1,1),padding='same', activation='relu'))
     
 #    model.add(MaxPooling2D(pool_size=(2, 2), strides=None, padding='same', data_format=None))
     
@@ -59,7 +64,7 @@ def mj_model():
     model.add(Dense(1164, activation='relu'))
     model.add(Dense(100, activation='relu'))
     model.add(Dense(50, activation='relu'))
-#    model.add(Dense(10, activation='relu'))  # need delete
+    model.add(Dense(10, activation='relu'))  # need delete
     model.add(Dense(1, activation=None))
     
     adam = Adam(lr=0.001, beta_1=0.9, beta_2=0.999, epsilon=None, decay=0.0, amsgrad=False)
@@ -77,8 +82,8 @@ json_string = model.to_json()
 open('models/model.json','w').write(json_string) 
 model.save_weights('models/model.h5') 
 
-
-
+lost=model.evaluate(x=X_test, y=y_test, batch_size=None, verbose=1, sample_weight=None, steps=None)
+print ('test loss: ', lost)
 
 
 
